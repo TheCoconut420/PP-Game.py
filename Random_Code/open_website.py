19758e17082f81f2839cd245a6fe38a421caf02a
Stefan The Coconut <57968327+TheCoconut420@users.noreply.github.com>
2022-07-11 22:50:53 +0200

    Untis Website

--- Random_Code/open_website.py
+++ Random_Code/open_website.py
@@ -16,17 +16,13 @@ Path = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(Path)
 action = ActionChains(driver)
 
-def login(site):
+def login(site, username, password):
     if site == "1":
-        username = "username"
-        password = "password"
         driver.get("https://hb.itslearning.com")
         driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Username_input").send_keys(username)
         driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Password_input").send_keys(password)
         driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_nativeLoginButton").click()
     elif site == "2":
-        username = "username"
-        password = "password"
         driver.get("https://cissa.webuntis.com/WebUntis/?school=SZ+II+Utbremen#/basic/login")
         driver.find_element(By.CLASS_NAME, "un-input-group__input").send_keys(username)
         driver.find_element(By.CLASS_NAME, "un-input-group__input").send_keys(Keys.TAB, password, Keys.TAB, Keys.TAB, Keys.ENTER)
@@ -34,4 +30,4 @@ def login(site):
         for i in range (0,4):
             action.send_keys(Keys.TAB).perform()
         action.send_keys(Keys.ENTER).perform()
-login(input("1 for hb.itslearning.com, 2 for cissa.webuntis.com: "))
+login(input("1 for hb.itslearning.com, 2 for cissa.webuntis.com: "), input("Username: "), input("Password: "))
